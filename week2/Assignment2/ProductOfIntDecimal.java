f6629591ecfa2618fa04db276841b97c28e31e89
agmona <76511800+agmona@users.noreply.github.com>
2022-02-24 13:06:07 +0530

    Add files via upload

---
+++ week2/Assignment2/ProductOfIntDecimal.java
@@ -0,0 +1,14 @@
+package feb15;
+
+public class ProductOfIntDecimal {
+
+	public static void main(String[] args) {
+		int n1 = 6;
+		float n2 = 8.2f;
+		float n3 = n1*n2;
+		System.out.println(n3);
+		
+
+	}
+
+}
